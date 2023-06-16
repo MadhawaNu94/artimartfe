758e7bc359c9389ce72fcd5b2f1eef780956d984
Madhawa Nuwan <116958907+MadhawaNu94@users.noreply.github.com>
2023-06-16 20:49:45 +0530

    complete without BE

--- src/scenes/checkout/Confirmation.jsx
+++ src/scenes/checkout/Confirmation.jsx
@@ -1,5 +1,17 @@
-const Confirmation=()=>{
-    return <div>Confirmation</div>;
-    };
-    
-    export default Confirmation;
+import { Box } from "@mui/material";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+
+const Confirmation = () => {
+  return (
+    <Box m="90px auto" width="80%" height="50vh">
+      <Alert severity="success">
+        <AlertTitle>Success</AlertTitle>
+        You have successfully made an Order —{" "}
+        <strong>Congrats on Making your Purchase</strong>
+      </Alert>
+    </Box>
+  );
+};
+
+export default Confirmation;
